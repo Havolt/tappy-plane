3b809b55431938fd27f942a029b0700b9281fe48
Mark <markfitz815@gmail.com>
2024-12-09 22:31:58 +0000

    Add death state

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 600.0
-const THRUST: float = -200.0
+# Physics constants
+const GRAVITY: float = 1000.0
+const THRUST: float = -350.0
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimSprite
 
 func _ready() -> void:
 	pass
@@ -9,13 +12,26 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	
+	# Apply gravity
 	velocity.y += GRAVITY * delta
 	
+	# Check for input and fly
 	fly()
 	
+	# Move the plane
 	move_and_slide()
-
+	
+	# Kill the plane if it hits the floor
+	if is_on_floor():
+		die()
+		
+# Flying logic when user presses space
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = THRUST
+
+# Death logic
+func die() -> void:
+	anim_sprite.stop()
+	set_physics_process(false)
+
