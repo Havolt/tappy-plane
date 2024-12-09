7c5c2cbffcf166f4235cb5d33a94856991df2886
Mark <markfitz815@gmail.com>
2024-12-09 22:04:57 +0000

    Add thrust ability to plane

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const GRAVITY: float = 600.0
-
+const THRUST: float = -200.0
 
 func _ready() -> void:
 	pass
@@ -12,6 +12,10 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.y += GRAVITY * delta
 	
-	print(velocity)
+	fly()
 	
 	move_and_slide()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly"):
+		velocity.y = THRUST
