acc7d5cb7da08bd57d9fca0884dc8a74b9e5791f
Mark <markfitz815@gmail.com>
2024-12-16 21:23:15 +0000

    Add high score to local storage

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -1,12 +1,14 @@
 extends Node
 
+const SCORES_PATH = "user://tappy.dat"
+
 var _score: int = 0
 var _high_score: int = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	load_high_score()
 
 func get_score() -> int:
 	return _score
@@ -22,3 +24,23 @@ func set_score(newScore: int) -> void:
 		
 func increment_score() -> void:
 	set_score(_score + 1)
+
+
+func load_high_score() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file:
+		if file.get_length() > 0:
+			_high_score = file.get_as_text().to_int()
+			print("Loaded Highscore")
+		else:
+			print("File empty")
+		file.close()
+	else: 
+		print("Failed to load file")
+		
+func save_high_score_to_file() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	if file:
+		#file.store_32(_high_score)
+		file.store_string(str(_high_score))
+		file.close()
