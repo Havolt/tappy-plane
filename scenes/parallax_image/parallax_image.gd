d855cfab8385796124f255d38b81c70043968323
Mark <markfitz815@gmail.com>
2024-12-18 21:42:02 +0000

    Trying out parallax2d node

---
+++ scenes/parallax_image/parallax_image.gd
@@ -0,0 +1,19 @@
+extends Parallax2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var texture: Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Scale image to squash to size of screen	
+	var scale_f: float = get_viewport_rect().size.y / texture.get_height()
+	sprite_2d.texture = texture
+	sprite_2d.scale = Vector2(scale_f, scale_f)
+	# How far to scroll before mirroring the image	
+	repeat_size.x = texture.get_width() * scale_f
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
